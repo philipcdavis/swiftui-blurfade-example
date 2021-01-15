17b17569aafa91fbc71f6820997eb50e723c0139
Philip Davis <philipcdavis11@gmail.com>
2021-01-15 14:34:05 -0500

    Initial Commit

---
+++ BlurFade/BlurFadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlurFadeApp.swift
+//  BlurFade
+//
+//  Created by Philip Davis on 1/15/21.
+//
+
+import SwiftUI
+
+@main
+struct BlurFadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
